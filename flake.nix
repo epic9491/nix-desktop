c0ba0918192fcb2fd57ba99958e17c8b75a6451b
epic9491 <epic9491@tutamail.com>
2025-11-18 18:45:52 -0600

    Initial sync with self hosted repo

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/nix/configuration.nix
+      ];
+    };
+  };
+}
---
+++ hosts/nix/home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "gumbo";
+  home.homeDirectory = "/home/gumbo";
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      hello = "echo hey";
+      ls = "eza";
+    };
+
+    initExtra = ''
+     # eval "$(starship init bash)"
+      export EZA_CONFIG_DIR="$HOME/.config/eza"
+      export EZA_ICONS_AUTO=1
+
+    '';
+  };
+
+  home.packages = with pkgs; [
+    # add per-user packages here
+  ];
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  ### ~/.config symlinks ###
+
+  # ~/.config/starship.toml
+  xdg.configFile."starship.toml".source = ./config/starship.toml;
+
+  # ~/.config/ghostty/config
+  xdg.configFile."ghostty/config".source = ./config/ghostty.config;
+
+  # ~/.config/eza/config
+  xdg.configFile."eza/theme.yml".source = ./config/eza.yml;  
+
+
+  home.file = {
+    # example:
+    # ".screenrc".source = ./dotfiles/screenrc;
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+}
